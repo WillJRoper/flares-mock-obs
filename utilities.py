8668eb15892b52f6a836efc1953a673bd160ff1a
wjr21 <wjr21@sussex.ac.uk>
2021-05-07 14:35:04 +0100

    Added sub arrays for image creation to aid speed

--- utilities.py
+++ utilities.py
@@ -378,10 +378,11 @@ def make_soft_img(pos, Ndim, i, j, imgrange, ls, smooth, sub_size=100, numThread
                               y_img + int(Ndim/sub_size) + 1
 
         # Compute the image
-        print(x, y, sml)
-        g = np.exp(-(((Gx[sub_xlow: sub_xhigh] - x) ** 2 + (Gy[sub_ylow: sub_yhigh] - y) ** 2) / (2.0 * sml ** 2)))
-        print(g.shape)
-        print(sub_xlow, sub_xhigh, sub_ylow, sub_yhigh)
+        g = np.exp(-(((Gx[sub_xlow: sub_xhigh,
+                       sub_ylow: sub_yhigh] - x) ** 2
+                      + (Gy[sub_xlow: sub_xhigh,
+                         sub_ylow: sub_yhigh] - y) ** 2)
+                     / (2.0 * sml ** 2)))
 
         # Get the sum of the gaussian
         gsum = np.sum(g)
@@ -389,8 +390,6 @@ def make_soft_img(pos, Ndim, i, j, imgrange, ls, smooth, sub_size=100, numThread
         # If there are stars within the image in this gaussian
         # add it to the image array
         if gsum > 0:
-            print(gsmooth_img[sub_xlow: sub_xhigh, sub_ylow: sub_yhigh].shape)
-            print(l)
             gsmooth_img[sub_xlow: sub_xhigh, sub_ylow: sub_yhigh] += g * l / gsum
 
     # gsmooth_img, xedges, yedges = np.histogram2d(pos[:, i], pos[:, j],
