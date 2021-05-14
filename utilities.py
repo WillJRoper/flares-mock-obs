4ac97aeb422e572e15e0e7bcc7c74e2a73d5a53d
wjr21 <wjr21@sussex.ac.uk>
2021-05-14 20:41:22 +0100

    added plotting for occupancy

--- utilities.py
+++ utilities.py
@@ -410,6 +410,7 @@ def make_subfind_spline_img(pos, Ndim, i, j, tree, ids, smooth, spline_cut_off=5
 
         # Place the kernel for this particle within the img
         pix_vals = np.unique(smooth_img[pix_pos[inds, 0], pix_pos[inds, 1]])
+        print(pix_vals)
         for i in pix_vals:
             okinds = smooth_img[pix_pos[inds, 0], pix_pos[inds, 1]] == i
             if i == 0.0:
