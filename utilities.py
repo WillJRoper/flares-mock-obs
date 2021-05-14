dbdc52f3468a979053758e421d0b2fce6b689f77
wjr21 <wjr21@sussex.ac.uk>
2021-05-14 17:58:06 +0100

    added plotting for occupancy

--- utilities.py
+++ utilities.py
@@ -695,10 +695,7 @@ def noisy_img(img, snr, seed=10000):
     source = img[segm.data == segm.data[np.unravel_index(np.argmax(img),
                                                          img.shape)]]
 
-    noise_sum = np.sum(source) / snr
-    noise_sig = noise_sum / np.sqrt(2 * np.pi)
-
-    print(noise_sum, noise_sig, np.sum(source))
+    noise_sig = np.sum(source) / source.size / snr
 
     # Create large array of random noise
     noise = np.random.normal(loc=0.0, scale=noise_sig, size=img.shape)
