f659fa45ce7ac730e0a1ec394d890f210e39eecf
wjr21 <wjr21@sussex.ac.uk>
2021-05-14 20:44:21 +0100

    added plotting for occupancy

--- utilities.py
+++ utilities.py
@@ -412,6 +412,7 @@ def make_subfind_spline_img(pos, Ndim, i, j, tree, ids, smooth, spline_cut_off=5
         pix_vals = np.unique(smooth_img[pix_pos[inds, 0], pix_pos[inds, 1]])
         for i in pix_vals:
             okinds = smooth_img[pix_pos[inds, 0], pix_pos[inds, 1]] == i
+            print(okinds)
             if i == 0.0:
                 smooth_img[pix_pos[inds, 0], pix_pos[inds, 1]][okinds] = grpsubgrp
             else:
