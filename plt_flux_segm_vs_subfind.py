8c2c9b61bab23ddb2c00bd3b3a5ba2d1b8255507
wjr21 <wjr21@sussex.ac.uk>
2021-06-11 15:53:38 +0100

    added kron flux and fixed plots

--- plt_flux_segm_vs_subfind.py
+++ plt_flux_segm_vs_subfind.py
@@ -181,26 +181,28 @@ for n_z in range(len(snaps)):
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
-        try:
-            all_flux_segm = np.concatenate(list(flux_segm_dict.values()))
-        except ValueError:
-            all_flux_segm = np.array([])
+        # try:
+        #     all_flux_segm = np.concatenate(list(flux_segm_dict.values()))
+        # except ValueError:
+        #     all_flux_segm = np.array([])
+        #
+        # if all_flux_segm.size > 0 and flux_subfind.size > 0:
+        #     bin_edges = np.logspace(
+        #         np.log10(np.min((all_flux_segm.min(), flux_subfind.min()))) - 0.5,
+        #         np.log10(np.max((all_flux_segm.max(), flux_subfind.max()))) + 0.5,
+        #         50)
+        # elif flux_subfind.size == 0 and all_flux_segm.size > 0:
+        #     bin_edges = np.logspace(np.log10(all_flux_segm.min()) - 0.5,
+        #                             np.log10(all_flux_segm.max()) + 0.5,
+        #                             50)
+        # elif all_flux_segm.size == 0 and flux_subfind.size > 0:
+        #     bin_edges = np.logspace(np.log10(flux_subfind.min()) - 0.5,
+        #                             np.log10(flux_subfind.max()) + 0.5,
+        #                             50)
+        # else:
+        #     continue
 
-        if all_flux_segm.size > 0 and flux_subfind.size > 0:
-            bin_edges = np.logspace(
-                np.log10(np.min((all_flux_segm.min(), flux_subfind.min()))) - 0.5,
-                np.log10(np.max((all_flux_segm.max(), flux_subfind.max()))) + 0.5,
-                50)
-        elif flux_subfind.size == 0 and all_flux_segm.size > 0:
-            bin_edges = np.logspace(np.log10(all_flux_segm.min()) - 0.5,
-                                    np.log10(all_flux_segm.max()) + 0.5,
-                                    50)
-        elif all_flux_segm.size == 0 and flux_subfind.size > 0:
-            bin_edges = np.logspace(np.log10(flux_subfind.min()) - 0.5,
-                                    np.log10(flux_subfind.max()) + 0.5,
-                                    50)
-        else:
-            continue
+        bin_edges = np.logspace(-3, 4, 75)
 
         bin_wid = bin_edges[1] - bin_edges[0]
         bin_cents = bin_edges[:-1] + (bin_wid / 2)
