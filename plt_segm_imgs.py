2282c1f79fd2bdb8d37eff579b1270cede6e0e0c
wjr21 <wjr21@sussex.ac.uk>
2021-06-22 15:49:04 +0100

    make catalog script

--- plt_segm_imgs.py
+++ plt_segm_imgs.py
@@ -148,8 +148,8 @@ for f in filters:
     if imgs.size == 0:
         continue
 
-    print(np.sum(imgs, axis=[1, 2]).shape, imgs.shape)
-    sinds = np.argsort(np.sum(imgs, axis=[1, 2]))[::-1]
+    print(np.sum(imgs, axis=(1, 2)).shape, imgs.shape)
+    sinds = np.argsort(np.sum(imgs, axis=(1, 2)))[::-1]
     create_img_ids = img_ids[sinds][:10]
     imgs = imgs[sinds]
 
