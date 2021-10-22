822d09e4ca1f81aebbc99e01db27896084f16b20
wjr21 <wjr21@sussex.ac.uk>
2021-10-22 20:26:52 +0100

    image grids

--- plt_img_grid.py
+++ plt_img_grid.py
@@ -109,7 +109,7 @@ while ind < n_img:
     all_imgs = np.array(list(img_dict.values()))
     img_norm = LogNorm(vmin=np.log10(vmin), vmax=np.log10(vmax))
     mimg_norm = LogNorm(vmax=np.log10(mass_vmax))
-
+    print(vmin, vmax, mass_vmax)
     fig = plt.figure()
     gs = gridspec.GridSpec(ncols=len(filters) + 2, nrows=len(depths),
                            width_ratios=(len(filters) + 1) * [15, ] + [1, ])
