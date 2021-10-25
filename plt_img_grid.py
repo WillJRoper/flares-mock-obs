f6840edc6adcf62c833b4201fd0e5afe3af901ef
wjr21 <wjr21@sussex.ac.uk>
2021-10-25 14:23:32 +0100

    image grids

--- plt_img_grid.py
+++ plt_img_grid.py
@@ -127,11 +127,11 @@ while ind < n_img:
                      dpi=all_imgs.shape[-1])
     gs = gridspec.GridSpec(ncols=len(filters) + 4, nrows=len(depths) + 1,
                            width_ratios=(len(filters) + 1)
-                                        * [10, ] + [2, 2, ],
+                                        * [10, ] + [2, 2, 2],
                            height_ratios=len(depths) * [1., ] + [1.5, ])
     gs1 = gridspec.GridSpec(ncols=len(filters) + 4, nrows=len(depths) + 1,
                             width_ratios=(len(filters) + 1)
-                                         * [10, ] + [2, 2, ],
+                                         * [10, ] + [2, 2, 2],
                             height_ratios=len(depths) * [1., ] + [1.5, ])
     gs.update(wspace=0.0, hspace=0.0)
     gs1.update(wspace=1.0, hspace=0.0)
