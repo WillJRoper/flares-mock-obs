ad929320eb39459a9f55b01400ece952422a0d3a
wjr21 <wjr21@sussex.ac.uk>
2021-10-26 13:55:11 +0100

    image grids

--- get_cat.sh
+++ get_cat.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH --ntasks 1 # The number of cores you need...
 #SBATCH --array=1-480
-#SBATCH -p cosma7 #or some other partition, e.g. cosma, cosma6, etc.
+#SBATCH -p cosma6 #or some other partition, e.g. cosma, cosma6, etc.
 #SBATCH -A dp004
 #SBATCH --cpus-per-task=1
 #SBATCH -J MOCK-CAT #Give it something meaningful.
 #SBATCH -o logs/output_cat_job.%A_%a.out
 #SBATCH -e logs/error_cat_job.%A_%a.err
-#SBATCH -t 72:00:00
+#SBATCH -t 12:00:00
 
 # Run the job from the following directory - change this to point to your own personal space on /lustre
 cd /cosma7/data/dp004/dc-rope1/FLARES/flares-mock-obs
